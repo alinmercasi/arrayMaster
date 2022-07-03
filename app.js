9065391a5456d00d730507f9ad6e3c591a9cc3f2
Alin <alinmercasi@gmail.com>
2022-07-03 17:20:50 +0200

    some more practice with some(), every(), find(), findIndex(), splice() and slice()

--- app.js
+++ app.js
@@ -369,3 +369,58 @@ const numTodos = array =>
     else user[current.userId]++
     return user
   }, {})
+
+// ** SECTION
+// From array cardio day 2 of javascript30
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+]
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 },
+]
+
+// Some and Every Checks
+const isGreaterThanNineteen = number => number > 19
+const getBirthYear = obj => obj.year
+const getAge = birthYear => new Date().getFullYear() - birthYear
+
+const getPeopleAge = array => array.map(getBirthYear).map(getAge)
+
+// Array.prototype.some()
+// is at least one person 19 or older?
+const someOlderThanNineteen = array =>
+  getPeopleAge(array).some(isGreaterThanNineteen)
+
+// Array.prototype.every()
+// is everyone 19 or older?
+const everyOlderThanNineteen = array =>
+  getPeopleAge(array).every(isGreaterThanNineteen)
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const getById = (array, id) => array.find(item => item.id === id)
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const getIndexByIdAndDelete = (array, id) => {
+  const index = array.findIndex(item => item.id === id)
+  // this is not a good practice, because it will modify the original array
+  // return array.splice(index, 1)
+  // instead we can create a new array and return it without touching the original array
+
+  const newArray = [...array.slice(0, index), ...array.slice(index + 1)]
+  return newArray
+}
